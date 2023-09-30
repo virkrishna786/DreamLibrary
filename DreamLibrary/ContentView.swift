d90479e4f568abd118f9fa02b46570354cab3801
Krishan Vir Singh <krishan.vir.singh01@sap.com>
2023-09-30 10:53:46 +0530

    Initial Commit

---
+++ DreamLibrary/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DreamLibrary
+//
+//  Created by Singh, Krishan Vir on 30/09/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DreamLibrary/DreamLibraryApp.swift
@@ -0,0 +1,17 @@
+//
+//  DreamLibraryApp.swift
+//  DreamLibrary
+//
+//  Created by Singh, Krishan Vir on 30/09/23.
+//
+
+import SwiftUI
+
+@main
+struct DreamLibraryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
